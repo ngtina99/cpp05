2334f3bdc897d87374e5600360ce91ffa1d24e2b
Tina <nguyen.valentina.99@gmail.com>
2025-01-05 23:23:20 +0100

    added xcpp05

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,9 +6,13 @@
 /*   By: ngtina1999 <ngtina1999@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 01:11:58 by ngtina1999        #+#    #+#             */
-/*   Updated: 2025/01/05 01:12:07 by ngtina1999       ###   ########.fr       */
+/*   Updated: 2025/01/05 23:20:37 by ngtina1999       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+Bureaucrat::GradeTooHighException
+
+
+Bureaucrat::GradeTooLowException
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,12 +6,34 @@
 /*   By: ngtina1999 <ngtina1999@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 01:12:13 by ngtina1999        #+#    #+#             */
-/*   Updated: 2025/01/05 01:12:51 by ngtina1999       ###   ########.fr       */
+/*   Updated: 2025/01/05 23:22:20 by ngtina1999       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
 
+#include <iostream>
+
+class Bureaucrat {
+	std::string const _name,
+	int grade;
+
+	getName();
+	getGrade();
+	increment
+	decrement
+	If
+the grade is out of range, both of them will throw the same exceptions as the constructor.
+
+
+ 
+}
+
+Remember. Since grade 1 is the highest one and 150 the lowest,
+incrementing a grade 3 should give a grade 2 to the bureaucrat.
+
+
+Any attempt to instantiate a Bureaucrat using an invalid grade must throw an exception
 
 #endif
