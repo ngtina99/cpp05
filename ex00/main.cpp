ee720324fac7c1c05e5acda5220064395ba202f9
Tina <nguyen.valentina.99@gmail.com>
2025-01-05 03:34:20 +0100

    added

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,12 +6,15 @@
 /*   By: ngtina1999 <ngtina1999@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 17:32:06 by ngtina1999        #+#    #+#             */
-/*   Updated: 2024/12/09 17:32:22 by ngtina1999       ###   ########.fr       */
+/*   Updated: 2025/01/05 02:08:21 by ngtina1999       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include ".h"
+#include "Bureaucrat.hpp"
+
+// Please note that exception classes don’t have to be designed in
+// Orthodox Canonical Form. But every other class has to.
 
 int	main() {
-	
+  		
 }
