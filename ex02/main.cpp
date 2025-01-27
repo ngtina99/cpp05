afb3733bcb7b3c5313719e4289b4b40e28c913dc
Tina <nguyen.valentina.99@gmail.com>
2025-01-27 03:24:17 +0100

    added

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngtina1999 <ngtina1999@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 17:32:06 by ngtina1999        #+#    #+#             */
-/*   Updated: 2025/01/27 03:10:17 by ngtina1999       ###   ########.fr       */
+/*   Updated: 2025/01/27 03:22:42 by ngtina1999       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,77 +26,84 @@
 
 int	main() {
 
-	Bureaucrat pedroB("Pedro", 2);
-	Bureaucrat JoaoB("Pedro", 2);
-	Bureaucrat MiguelB("Pedro", 2);
+	Bureaucrat pedroB("Pedro", 149);
 	std::cout << pedroB;
-	std::cout << JoaoB;
-	std::cout << MiguelB;
 
-	AForm formB("form1", 1, 1);
-	std::cout << formB;
+	RobotomyRequestForm formRobotomy("Robotomy");
 	try
 	{
-		std::cout << "Pedro with too low grade try to sign a form." << std::endl;
-		pedroB.signForm(formB);
+		std::cout << "Pedro with too low grade try to sign a RobotomyForm." << std::endl;
+		pedroB.signForm(formRobotomy);
+		pedroB.executeForm(formRobotomy);
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
-	AForm formC("form2", 2, 2);
-	std::cout << formC;
+	PresidentialPardonForm formPresidential("Presidential");
 	try
 	{
-		std::cout << "Pedro with equal grade try to sign a form." << std::endl;
-		pedroB.signForm(formC);
+		std::cout << "Pedro with too low grade try to sign a PresidentialPardonForm." << std::endl;
+		pedroB.signForm(formPresidential);
+		pedroB.executeForm(formPresidential);
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
-	AForm formD("form3", 3, 3);
-	std::cout << formD;
+	ShrubberyCreationForm formShrubbery("Shrubbery");
 	try
 	{
-		std::cout << "Pedro with a higher grade try to sign a form." << std::endl;
-		pedroB.signForm(formD);
+		std::cout << "Pedro with too low grade try to sign a ShrubberyCreationForm." << std::endl;
+		pedroB.signForm(formShrubbery);
+		pedroB.executeForm(formShrubbery);
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
-		try
-	{
-		std::cout << "Pedro with too low grade try to sign a form." << std::endl;
-		pedroB.signForm(formB);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	AForm formC("form2", 2, 2);
-	std::cout << formC;
+
+	Bureaucrat JoaoB("Joao", 1);
+	std::cout << JoaoB;
+
+	RobotomyRequestForm formRobotomy2("Robotomy2");
 	try
 	{
-		std::cout << "Pedro with equal grade try to sign a form." << std::endl;
-		pedroB.signForm(formC);
+		std::cout << "Joao execute a RobotomyForm 4 times in a row." << std::endl;
+		JoaoB.signForm(formRobotomy2);
+		JoaoB.executeForm(formRobotomy2);
+		JoaoB.executeForm(formRobotomy2);
+		JoaoB.executeForm(formRobotomy2);
+		JoaoB.executeForm(formRobotomy2);
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
-	AForm formD("form3", 3, 3);
-	std::cout << formD;
+	PresidentialPardonForm formPresidential2("Presidential2");
 	try
 	{
-		std::cout << "Pedro with a higher grade try to sign a form." << std::endl;
-		pedroB.signForm(formD);
+		std::cout << "Joao execute a PresidentialPardonForm." << std::endl;
+		JoaoB.signForm(formPresidential2);
+		JoaoB.executeForm(formPresidential2);
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
+	ShrubberyCreationForm formShrubbery2("Shrubbery2");
+	try
+	{
+		std::cout << "Joao execute a ShrubberyCreationForm." << std::endl;
+		JoaoB.signForm(formShrubbery2);
+		JoaoB.executeForm(formShrubbery2);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	
 	// std::cout << std::cout << "\e[1;1H\e[2J";
 	// try
 	// {
